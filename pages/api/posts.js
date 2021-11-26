ea00d060c566173e17d36e7b4ab79596bd0bf388
JuLblc <julien.leblanc@epfedu.fr>
2021-11-26 15:02:51 +0100

    CRUD completed

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -66,3 +66,56 @@ async function addPost(req, res) {
     });
   }
 }
+
+async function updatePost(req, res) {
+  try {
+    // connect to the database
+    let { db } = await connectToDatabase();
+
+    // update the published status of the post
+    await db.collection('posts').updateOne(
+      {
+        _id: new ObjectId(req.body),
+      },
+      { $set: { published: true } }
+    );
+
+    // return a message
+    return res.json({
+      message: 'Post updated successfully',
+      success: true,
+    });
+  } catch (error) {
+
+    // return an error
+    return res.json({
+      message: new Error(error).message,
+      success: false,
+    });
+  }
+}
+
+async function deletePost(req, res) {
+  try {
+      // Connecting to the database
+      let { db } = await connectToDatabase();
+
+      // Deleting the post
+      await db.collection('posts').deleteOne({
+          _id: new ObjectId(req.body),
+      });
+
+      // returning a message
+      return res.json({
+          message: 'Post deleted successfully',
+          success: true,
+      });
+  } catch (error) {
+
+      // returning an error
+      return res.json({
+          message: new Error(error).message,
+          success: false,
+      });
+  }
+}
