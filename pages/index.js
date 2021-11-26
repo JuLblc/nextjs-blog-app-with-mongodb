04931a2f7b2793145085a8c9215582b7579a281b
JuLblc <julien.leblanc@epfedu.fr>
2021-11-26 12:12:48 +0100

    Setting up the API route, fetching and adding posts

--- pages/index.js
+++ pages/index.js
@@ -35,9 +35,18 @@ export default Home;
 
 export async function getServerSideProps(ctx) {
 
+  // get the current environment
+  let dev = process.env.NODE_ENV !== 'production';
+  let { DEV_URL, PROD_URL } = process.env;
+
+  // request posts from api
+  let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/posts`);
+  // extract the data
+  let data = await response.json();
+
   return {
-      props: {
-          posts: ['message'],
-      },
+    props: {
+      posts: data['message'],
+    },
   };
 }
